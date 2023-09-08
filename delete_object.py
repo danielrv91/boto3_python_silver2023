bd314417006d0376b9fcee03ba136797b24b3647
danielrv91 <rosalesdaniel91@gmail.com>
2023-09-08 21:41:12 +0000

    delete-object

--- delete_object.py
+++ delete_object.py
@@ -1,12 +1,17 @@
+# Import the 'boto3' library, which is a Python SDK for Amazon Web Services (AWS).
 import boto3
 
+# Define variables for the S3 bucket and object key to be deleted.
 bucket = 'drosales-boto3-09062023'
 key = 'test_text.txt'
 
+# Create an AWS S3 client object using boto3.
 s3 = boto3.client('s3')
 
+# Use the S3 client to send a request to delete the specified object from the specified bucket.
 response = s3.delete_object(
     Bucket=bucket,
     Key=key,
-  
 )
+
+# The 'response' variable may contain confirmation or error information.
